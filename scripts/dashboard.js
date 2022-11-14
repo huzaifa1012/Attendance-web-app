e0433963e38ecf282d1ebcb2ce795bbb16e8a894
huzaifa1012 <www.pakteach@gmail.com>
2022-11-14 23:39:41 +0500

    dashboard completed

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -2,23 +2,32 @@ import { app, auth, db, storage } from "./firebaseconfig.js"
 import { collection, query, where, onSnapshot } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js";
 
 
-let fetchStudent = document.querySelector("#batch")
+let fetchStudent = document.querySelector("#course")
 let mainsec = document.querySelector(".main-sec")
 let get = document.querySelector(".button")
 get.addEventListener("click", fetchStud)
+
 function fetchStud() {
-alert ("asd")
-    const q = query(collection(db, "students"), where("course", "==", "Web & Mobile" ));
+    if(fetchStudent.value == "Select Class"){
+        swal("Please Select Course!", "Particular ");
+return
+    }
+    console.log(fetchStudent.value);
+    mainsec.innerHTML = "" 
+
+    const q = query(collection(db, "students"), where("course", "==", fetchStudent.value ));
     const unsubscribe = onSnapshot(q, (querySnapshot) => {
         querySnapshot.forEach((doc) => {
             let data = doc.data()
 //           
 console.log(data.name)
+
 mainsec.innerHTML += 
             `
-            
             <div class="backbone">
+           <div class="img-wrapper">
             <img src="${data.img}" alt="" id="prof-img">
+            </div>
             <h3 id="userName">${data.name} ${data.father}</h3>
             <p id="cnic">${data.cnic}</p>
             <p id="phone">${data.phone}</p>
